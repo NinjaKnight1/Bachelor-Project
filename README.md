6478ab19095d82d90772d386ba3a63995cb9e6d4
Hannahtersbol <s224775@dtu.dk>
2025-03-19 09:05:33 +0100

    BPMN to Petri_Net works

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ You first have to download the nodes modules you have to run
 
 ```
 npm install
-pip install equirements.txt
+pip install requirements.txt
+
+graphviz also needs to be installed 
 ```
 
 To run the project you have to run
--- bpmn-dpn/api.py
+++ bpmn-dpn/api.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import pm4py
+from pm4py.visualization.petri_net import visualizer as pn_vis_factory
 
 app = FastAPI()
 
@@ -35,7 +36,16 @@ async def convert_bpmn(file: UploadFile = File(...)):
         bpmn_model = pm4py.read_bpmn(file_path)
 
         # Convert BPMN to Petri Net
-        petri_net, im, fm = pm4py.convert_to_petri_net(bpmn_model)
+        petri_net, im, fm = pm4py.objects.conversion.bpmn.variants.to_petri_net.apply(bpmn_model)
+
+        # Export the Petri net to PNML using the exporter
+        pm4py.write_pnml(petri_net, im, fm, file_path.replace(".bpmn", ".pnml"))
+
+        # Convert the Petri Net to a DiGraph
+        # Visualize the Petri Net and save the diagram
+        gviz = pn_vis_factory.apply(petri_net, im, fm)
+        diagram_path = file_path.replace(".bpmn", ".png")
+        pn_vis_factory.save(gviz, diagram_path)
 
         return {"message": "Conversion successful!", "file_path": file_path}
 
